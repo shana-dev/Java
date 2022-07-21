7b32d4ccfbbc4ca45d3233474366f4afa9f5d534
Shana <76134739+shana-dev@users.noreply.github.com>
2022-07-21 15:03:29 +0200

    Add files via upload

---
+++ coreJava/src/main/java/Initials.java
@@ -0,0 +1,12 @@
+public class Initials {
+
+    public static void main(String[] args) {
+        System.out.println("N       N       A") ;
+        System.out.println("N N     N     A    A ") ;
+        System.out.println("N  N    N   A        A ") ;
+        System.out.println("N   N   N   A A A A  A  ") ;
+        System.out.println("N     N N   A        A  ") ;
+        System.out.println("N       N   A        A  ") ;
+        System.out.println("N       N   A        A  ") ;
+    }
+}
---
+++ coreJava/src/main/java/Section2/Variables.java
@@ -0,0 +1,10 @@
+package Section2;
+
+public class Variables {
+    public static void main(String[] args) {
+     int passengers = 20;
+     System.out.println(passengers);
+     int busTickets =  passengers;
+     System.out.println(busTickets);
+    }
+}
